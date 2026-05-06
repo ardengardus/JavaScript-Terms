f21988dce2b98178403d6ca210d7cfa97b902a00
ardengardus <arsen.grigoryan.2510@gmail.com>
2026-05-06 10:29:02 +0300

    Update

--- Tern2-ifelse.js
+++ Tern2-ifelse.js
@@ -5,27 +5,29 @@ function manyChecks() {
   let result = '';
   
   if (a > 10) {
-    result = 'a > 10';
+    result = 'a is bigger than 10';
   } else {
     if (a === 5) {
-      result = 'a <= 10 - пример особого случая';
+      result = 'a is less than or equal to 10 an example of a special case';
     } else {
-      result = 'a <= 10 ';
+      result = 'a is less than or equal to 10 ';
     }
   }
   
   if (a === 15) {
-    result += 'но a не равно 15';
+    result += 'but a is not 15';
   }
+  
   if (a > 5) {
-    result += 'и a > 5';
+    result += 'and a is greater than 5';
   } else {
-    result += 'и a <= 5 ';
+    result += 'and a is less than or equal to 5 ';
   }
+  
   if (a % 2) {
-    result += ' и a - нечётное';
+    result += ' and a is odd';
   } else {
-    result += ' и a - чётное';
+    result += ' and a is even';
   }
   
   console.log(result);
--- Tern2-switch.js
+++ Tern2-switch.js
@@ -6,39 +6,39 @@ function manyChecks() {
   
   switch (true) {
     case a > 10:
-      result = 'a > 10';
+      result = 'a is bigger than 10';
       break;
     default:
       switch (a) {
         case 5:
-          result = 'a <= 10 - пример особого случая';
+          result = 'a is less than or equal to 10 an example of a special case';
           break;
         default:
-          result = 'a <= 10 ';
+          result = 'a is less than or equal to 10 ';
       }
   }
   
   switch (a) {
     case 15:
-      result += 'но a не равно 15';
+      result += 'but a is not 15';
       break;
     default:
   }
   
   switch (true) {
     case a > 5:
-      result += 'и a > 5';
+      result += 'and a is greater than 5';
       break;
     default:
-      result += 'и a <= 5';
+      result += 'and a is less than or equal to 5 ';
   }
   
   switch (a % 2) {
     case 1:
-      result += ' и a - нечётное';
+      result += ' and a is odd';
       break;
     case 0:
-      result += ' и a - чётное';
+      result += ' and a is even';
       break;
   }
   
